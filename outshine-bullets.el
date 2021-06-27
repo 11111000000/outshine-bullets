b3aaa707cc7c3652a80bc1b587ba1bae8b3f3e39
Peter <11111000000@email.com>
2021-06-27 18:27:01 +0000

    Update outshine-bullets.el

--- outshine-bullets.el
+++ outshine-bullets.el
@@ -25,15 +25,6 @@
 
 ;; Show outshine-mode bullets as UTF-8 characters.
 
-;; This is a legacy package maintained with a focus on preservation.
-;; It has an unofficial successor package (outshine-superstar).  This means
-;; that new features will no longer be added, and backwards
-;; compatibility will be preserved.
-
-;; It's unofficial successor package is available on MELPA.  You can
-;; also find it on GitHub:
-;; https://github.com/integral-dw/outshine-superstar-mode
-
 ;;; Code:
 
 (require 'outshine)
