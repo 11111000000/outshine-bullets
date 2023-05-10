2ce414869faf959d98d0bf09db2d82c69f3a832b
Petr Kosov <11111000000@email.com>
2023-05-11 04:49:03 +0800

    fix bullet margin

--- outshine-bullets-pkg.el
+++ outshine-bullets-pkg.el
@@ -1,2 +1,2 @@
-;;; Generated package description from /home/az/.emacs.d/elpa/org-bullets-20200317.1740/org-bullets.el  -*- no-byte-compile: t -*-
-(define-package "outshine-bullets" "20200317.1740" "Show bullets in outshine-mode as UTF-8 characters" 'nil :commit "767f55feb58b840a5a04eabfc3fbbf0d257c4792" :authors '(("sabof" "Peter Kosov")) :maintainer '("Peter Kosov" . "11111000000@email.com") :url "https://github.com/11111000000/outshine-bullets")
+;;; Generated package description from outshine-bullets.el  -*- no-byte-compile: t -*-
+(define-package "outshine-bullets" "2021.1111" "No description available." 'nil :kind vc)
--- outshine-bullets.el
+++ outshine-bullets.el
@@ -73,17 +73,17 @@ Otherwise the face of the heading level is used."
           ;;          (outshine-bullets-level-char level)
           ;;          (match-end 2)
           ;;          )
-          (compose-region (- (match-end 0) 1)
-                          (- (match-end 0) 0)
+          (compose-region (- (match-end 0) 2)
+                          (- (match-end 0) 1)
                           (outshine-bullets-level-char level))
           (when (facep outshine-bullets-face-name)
-            (put-text-property (- (match-end 0) 1)
-                               (- (match-end 0) 0)
+            (put-text-property (- (match-end 0) 2)
+                               (- (match-end 0) 1)
                                'face
                                outshine-bullets-face-name))
           
           (put-text-property (match-beginning 0)
-                             (- (match-end 0) 1)
+                             (- (match-end 0) 2)
                              'face 'org-hide)
           
           ;; (put-text-property (match-beginning 0)
